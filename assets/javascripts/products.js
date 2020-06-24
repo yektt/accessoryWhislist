5329683465faa09ee27ae568ed406095c4799f75
Yekta <r.yekt.93@gmail.com>
2020-06-24 10:13:01 +0200

    changing the names from 'hat' to 'accessory'
    making the names global

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -1,17 +1,17 @@
-// construction function for Hats
-function Hat(name, price, color, imageHref) {
+// construction function for Accessories
+function Accessory(name, price, color, imageHref) {
   this.name = name,
   this.price = price,
   this.color = color,
   this.imageHref = imageHref
 }
 
-Hat.prototype.toString = function () {
+Accessory.prototype.toString = function () {
   return 'name <' + this.name + '>, color <' + this.color + '>, price <' + this.price + '>, image: <' + this.imageHref + '>';
 }
 
-// it's taking one hat object and creating HTML component
-function displayHat(hat) {
+// it's taking one accessory object and creating HTML component
+function displayAccessory(accessory) {
   let button = document.createElement('button');
   button.className = 'btn btn-outline-primary';
   button.textContent = 'Add to whislist!';
@@ -21,32 +21,32 @@ function displayHat(hat) {
   paragraph.textContent = 'Color: '
 
   let em = document.createElement('em');
-  em.textContent = hat.color;
+  em.textContent = accessory.color;
 
   let h5 = document.createElement('h5');
   h5.className = 'card-title';
-  h5.textContent = hat.name;
+  h5.textContent = accessory.name;
 
   let card_body = document.createElement('div');
   card_body.className = 'card-body text-center';
 
   let image = document.createElement('img');
   image.className = 'card-img-top';
-  image.src = hat.imageHref;
-  image.alt = 'Image of ' + hat.name.toLowerCase();
+  image.src = accessory.imageHref;
+  image.alt = 'Image of ' + accessory.name.toLowerCase();
 
   let currency = document.createElement('div');
   currency.className = 'currency btn btn-light disabled'
-  currency.textContent = hat.price;
+  currency.textContent = accessory.price;
 
   let card = document.createElement('div');
   card.className = 'card my-3';
 
-  let accessory = document.createElement('div');
-  accessory.className = 'accessory col-sm-4 fromData';
-  accessory.classList.add(hat.color);
+  let div = document.createElement('div');
+  div.className = 'accessory col-sm-4 fromData';
+  div.classList.add(accessory.color);
 
-  accessory.appendChild(card);
+  div.appendChild(card);
   card.appendChild(currency);
   card.appendChild(image);
   card.appendChild(card_body);
@@ -56,7 +56,7 @@ function displayHat(hat) {
   paragraph.appendChild(em);
 
   let products = document.getElementById('products');
-  products.appendChild(accessory);
+  products.appendChild(div);
 }
 
 // HatNodeList -> from static HTML
@@ -75,14 +75,14 @@ for (let i = 0; i < HatsNodeList.length; i++) {
   HatsNodeList[i].style.display = 'none';
 
   // creating a Hat object and adding it to the HatArray for storing the hats-data
-  const hat = new Hat(HTMLname, HTMLprice, HTMLcolor, HTMLimageHref);
+  const hat = new Accessory(HTMLname, HTMLprice, HTMLcolor, HTMLimageHref);
 
   HatArray.push(hat);
 }
 
 // for rendering all of the hat objects.
 for (let i = 0; i < HatArray.length; i++) {
-  displayHat(HatArray[i]);
+  displayAccessory(HatArray[i]);
 }
 
 /* ----------------- Filter By Color ----------------- */
@@ -99,28 +99,29 @@ const filterByColorNodeList = document.querySelectorAll('.btn-group button');
 
 // for changing 'active' class for color filter button
 function highlightSelectedFilter(button) {
+  
   for (let i = 0; i < filterByColorNodeList.length; i++) {
     filterByColorNodeList[i].classList.remove('active');
+    filterAccessoriesByColor(button.textContent);
   }
   button.classList.add('active');
-  filterHatByColor(button.textContent);
 }
 
-// Listening to all of the filter buttons to detect whether any of them is clicked.  
-filterByColorNodeList.forEach(function (filterButton) {
-  filterButton.addEventListener('click', function () {
-    highlightSelectedFilter(filterButton);
+// Listening to all of the color-filter buttons to detect whether any of them is clicked.  
+filterByColorNodeList.forEach(function (colorFilterButton) {
+  colorFilterButton.addEventListener('click', function () {
+    highlightSelectedFilter(colorFilterButton);
   });
 });
 
-function filterHatByColor(filter) {
+function filterAccessoriesByColor(filter) {
   let HTMLNodeList = document.querySelectorAll('.fromData');
-  // all hats will be removed from page
+  // all accessories will be removed from page
   for (let i = 0; i < HTMLNodeList.length; i++) {
     HTMLNodeList[i].style.display = 'none';
   }
 
-  // if the hats' color is choosen filter, they will display on the page
+  // if the accoessories' color is the same with choosen filter, they will display on the page
   for (let i = 0; i < HTMLNodeList.length; i++) {
     if (HTMLNodeList[i].classList.contains(filter.toLowerCase())) {
       HTMLNodeList[i].style.display = 'inline';
@@ -129,3 +130,4 @@ function filterHatByColor(filter) {
     }
   }
 }
+
